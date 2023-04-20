ac1379e3323ff5d4708704fbb343b71dd9a65337
Yosef <yosethio@yahoo.com>
2023-04-20 07:52:40 +0300

    Resolved merge conflict in ./pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import { AppProps } from "next/app";
-=======
-import { AppProps } from 'next/app';
-import '../styles/index.css';
-import '../styles/audio.css';
->>>>>>> aa39fc7f3381656a14f872abda7274d19778faf0
+import "../styles/index.css";
+import "../styles/audio.css";
 
 import "../styles/index.css";
 import "../styles/audio.css";
