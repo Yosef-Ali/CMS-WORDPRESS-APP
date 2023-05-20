052812796f5d837414cc3f903e468bae4eb8b1f2
Yosef <yosethio@yahoo.com>
2023-05-20 11:31:59 +0300

    nest config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,28 @@
+if (!process.env.WORDPRESS_API_URL) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `);
+}
+
+/** @type {import('next').NextConfig} */
 module.exports = {
   images: {
     domains: [
-      "ethiocatholicaddis.org", // WordPress domain
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
       "0.gravatar.com",
       "1.gravatar.com",
       "2.gravatar.com",
       "secure.gravatar.com",
     ],
   },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_KEY: process.env.WORDPRESS_API_URL,
+  },
+  distDir: "build",
+
   env: {
     WORDPRESS_API_URL: "https://ethiocatholicaddis.org/acs-cms/graphql",
   },
-  distDir: "build",
 };
