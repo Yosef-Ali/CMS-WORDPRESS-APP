b9889c515d595910bb13774cb89d5e0402fc2fd5
Yosef <yosethio@yahoo.com>
2023-05-22 07:52:40 +0300

    next config.js google update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,37 @@
-if (!process.env.WORDPRESS_API_URL) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `);
-}
+// if (!process.env.WORDPRESS_API_URL) {
+//   throw new Error(`
+//     Please provide a valid WordPress instance URL.
+//     Add to your environment variables WORDPRESS_API_URL.
+//   `);
+// }
 
-/** @type {import('next').NextConfig} */
-module.exports = {
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   images: {
+//     domains: [
+//       process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+//       "0.gravatar.com",
+//       "1.gravatar.com",
+//       "2.gravatar.com",
+//       "secure.gravatar.com",
+//     ],
+//   },
+//   publicRuntimeConfig: {
+//     // Will be available on both server and client
+//     API_KEY: process.env.WORDPRESS_API_URL,
+//   },
+
+//   env: {
+//     WORDPRESS_API_URL: "https://ethiocatholicaddis.org/acs-cms/graphql",
+//   },
+// };
+
+const { withImages, withPublicRuntimeConfig } = require("next-images");
+
+const nextConfig = {
   images: {
     domains: [
-      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      "https://ethiocatholicaddis.org",
       "0.gravatar.com",
       "1.gravatar.com",
       "2.gravatar.com",
@@ -18,10 +40,8 @@ module.exports = {
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
-    API_KEY: process.env.WORDPRESS_API_URL,
-  },
-
-  env: {
-    WORDPRESS_API_URL: "https://ethiocatholicaddis.org/acs-cms/graphql",
+    API_KEY: "https://ethiocatholicaddis.org/acs-cms/graphql",
   },
 };
+
+module.exports = withImages(withPublicRuntimeConfig(nextConfig));
